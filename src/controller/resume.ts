1b2491b1f1fe9fad585c46c4691719c3bd637121
VineeTagarwaL-code <vineetagarwal.now@gmail.com>
2024-10-07 21:02:54 +0530

    fix: dirname added

--- src/controller/resume.ts
+++ src/controller/resume.ts
@@ -4,21 +4,19 @@ import fs from "fs";
 import pdfParse from "pdf-parse";
 import mammoth from "mammoth";
 import { sendToGemini } from "../gemini";
-
+const fileSourcePath = __dirname + "/uploads";
+// how to get the root path
+const rootPath = path.resolve(__dirname, "../../");
+console.log(rootPath);
 const getResumeJson = async (req: Request, res: Response) => {
   if (!req.file) {
     return res.status(400).json({ error: "No file uploaded" });
   }
-  const filePath = path.join(
-    "/home/vineet/Development/projects/work/100xdevs/resume-parser",
-    "uploads",
-    req.file.filename
-  );
+  const filePath = path.join(rootPath, "uploads", req.file.filename);
+  console.log(filePath);
   const fileExt = path.extname(req.file.originalname).toLowerCase();
-  const uploadDir = path.join(
-    "/home/vineet/Development/projects/work/100xdevs/resume-parser",
-    "uploads"
-  );
+  const uploadDir = path.join(rootPath, "uploads");
+  console.log(uploadDir);
   try {
     let extractedText = "";
     if (!fs.existsSync(uploadDir)) {
